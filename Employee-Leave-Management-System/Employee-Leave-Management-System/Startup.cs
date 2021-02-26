8947051e8e6151c2038a3d9721426fc1c8f05b98
DariuszKitel <dariuszkitel@gmail.com>
2021-02-26 05:48:26 +0100

    Added dataTables and fontAwesome for better view

--- Employee-Leave-Management-System/Employee-Leave-Management-System/Startup.cs
+++ Employee-Leave-Management-System/Employee-Leave-Management-System/Startup.cs
@@ -45,7 +45,7 @@ namespace Employee_Leave_Management_System
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
