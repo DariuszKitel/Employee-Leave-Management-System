764d53900ce2e44e28e7fd16605acd9fb86f26c9
DariuszKitel <dariuszkitel@gmail.com>
2023-04-02 00:27:57 +0200

    Created initial empty project

---
+++ Employee-Leave-Management-System.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Leave_Management_System.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Employee-Leave-Management-System.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Employee_Leave_Management_System.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
